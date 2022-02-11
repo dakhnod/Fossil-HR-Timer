183a3f36a8a5085e5bcea6bb7b90116e6b5a87e8
Daniel Dakhno <dakhnod@gmail.com>
2022-02-11 02:55:31 +0100

    fixed full_redraw on newer firmwares

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ return {
         }
         response.draw_screen = function (node_name, full_update, layout_info) {
             response.draw = {
-                update_type: full_update ? 'du4' : 'gu4'
+                update_type: full_update ? 'gc4' : 'du4'
             }
             response.draw[node_name] = {
                 layout_function: 'layout_parser_json',
